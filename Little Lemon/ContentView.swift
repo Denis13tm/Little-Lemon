cf2d48149f8121cbb420b5e008ca597fb0299658
Denis13tm <otawflash@gmail.com>
2024-05-06 12:03:08 +0900

    Initial Commit

---
+++ Little Lemon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Little Lemon
+//
+//  Created by Otabek Tuychiev on 06/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
